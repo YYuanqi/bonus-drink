b02593704818f21f04da65f2b575b43c0c6ae1c6
yun.yuanqi <yun.yuanqi@jp.fujitsu.com>
2019-06-26 16:04:33 +0900

    added solution

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,9 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    if amount >= 3
+      amount + amount/3 + total_count_for(amount/3 + amount%3)/3
+    else
+      amount
+    end
   end
 end
